efe097f42a44220237f92609ce1e2eab6346535f
Graham Ollis <plicease@cpan.org>
2019-01-14 20:06:06 -0500

    move to Perl5-FFI org

--- lib/Module/Build/FFI/Pascal.pm
+++ lib/Module/Build/FFI/Pascal.pm
@@ -262,14 +262,14 @@ If something does not work as advertised, or the way that you think it
 should, or if you have a feature request, please open an issue on this
 project's GitHub issue tracker:
 
-L<https://github.com/plicease/FFI-Platypus-Lang-Pascal/issues>
+L<https://github.com/Perl5-FFI/FFI-Platypus-Lang-Pascal/issues>
 
 =head1 CONTRIBUTING
 
 If you have implemented a new feature or fixed a bug then you may make a
 pull reequest on this project's GitHub repository:
 
-L<https://github.com/plicease/FFI-Platypus-Lang-Pascal/issues>
+L<https://github.com/Perl5-FFI/FFI-Platypus-Lang-Pascal/issues>
 
 Caution: if you do this too frequently I may nominate you as the new
 maintainer.  Extreme caution: if you like that sort of thing.
